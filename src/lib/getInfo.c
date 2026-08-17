d4f4a865835c5a086a4f3ae6ce168dd0243d0d82
iamadvisory <olegbrevnov772@gmail.com>
2026-08-17 16:54:49 +0600

    added v0.8 screen

--- src/lib/getInfo.c
+++ src/lib/getInfo.c
@@ -245,12 +245,6 @@ void getCpu(char *buffer, size_t size) {
     fclose(fp);
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
-#include <strings.h>
-
 void getGpu(char *buffer, size_t size) {
     snprintf(buffer, size, "Unknown");
 
